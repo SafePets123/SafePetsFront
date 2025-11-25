f81bd176b4ad8f4096c134fa1ecfa821f21c7f61
Fernando <valentinfernando38@gmail.com>
2025-11-25 16:11:08 -0300

    oie

--- ReactTCC/src/components/DenunciasRecebidas.tsx
+++ ReactTCC/src/components/DenunciasRecebidas.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import DenunciaCardAutoridade from "./DenunciaCardAutoridade"; 
+import DenunciaCardAutoridade from "../components/DenunciaCardAutoridade"; 
 
 // Interfaces de dados
 interface DenunciaParaCard {
